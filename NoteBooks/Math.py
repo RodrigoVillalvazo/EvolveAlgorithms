67bfd8cc800e8be95f2272c7c2ed05cb3ced2ee7
Rodrigo Villalvazo <rodri.villalvazo3@gmail.com>
2024-10-08 21:32:17 -0700

    Add new feature
    Now the notebook version of google colab is online

---
+++ NoteBooks/Math.py
@@ -0,0 +1,447 @@
+import numpy as np
+import math as mat
+import cmath as ctm
+import warnings
+from sympy import trigsimp
+from scipy import integrate
+from scipy import fft
+warnings.filterwarnings("ignore")
+np.seterr(all='warn')
+nan=mat.nan
+Inf=mat.inf
+def Integrate(x,y):
+    return(integrate.fixed_quad(x,y,None))
+def FFT(y,z):
+    return(fft(y,z))
+def TrigSimp(y):
+    return(trigsimp(y))
+def Transltr(val0):
+    if isinstance(val0,(float,int)):
+        return val0
+    else:
+        try:
+            ans=eval(val0.replace('"', '').replace("'", ''))
+            for a in ans:
+                if isinstance(a,(int,float)):
+                    return a
+                else:
+                    return ans
+        except:
+            return float(val0.replace('"', '').replace("'", ''))
+        #finally:
+         #   return str(val0.replace('"', '').replace("'", ''))
+def Negate(val1):
+    try:
+        return -val1
+    except:
+        for item in val1:
+            try:
+                b=-float(item)
+                return b
+            except:
+                return 1       
+def Minus(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        try:
+            return [a - b for a, b in zip(x, y)]
+        except:
+            return x - y
+    else:
+        return x - y
+
+def Plus(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        try:
+            return [a + b for a, b in zip(x, y)]
+        except:
+            return x + y
+    else:
+        return x + y
+
+def Times(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        try:
+            return [a * b for a, b in zip(x, y)]
+        except:
+            return x * y
+    else:
+        return x * y
+
+def Power(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        try:
+            return [a ** b for a, b in zip(Real(x), Real(y))]
+        except:
+            return nan
+    else:
+        return x ** y
+
+def Divide(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        return [a / b if b != 0 else np.nan for a, b in zip(x, y)]
+    else:
+        return (x / y) if y !=0 else np.nan
+
+def Maximum(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    else:
+        x=Real(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    else:
+        y= Real(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        return max([max(a,b) for a, b in zip(x, y)])
+    else:
+        return max(x, y)
+
+def Minimum(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    else:
+        x=Real(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    else:
+        y= Real(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        return min([min(a, b) for a, b in zip(x, y)])
+    else:
+        try:
+            return min(x, y)
+        except:
+            return nan
+
+def Atan2r(x, y):
+    if isinstance(x, str):
+        x = Transltr(x)
+    if isinstance(y, str):
+        y = Transltr(y)
+    if isinstance(x, list) or isinstance(y, list):
+        if isinstance(x, (float, int)):
+            x = [x] * len(y)
+        if isinstance(y, (float, int)):
+            y = [y] * len(x)
+        return [np.arctan2(np.real(a), np.real(b)) for a, b in zip(x, y)]
+        #return [np.arctan2(np.real(a), np.real(b)) if isinstance(a, complex) and isinstance(b, complex) else np.nan for a, b in zip(x, y)]
+    else:
+        return np.arctan2(np.real(x), np.real(y)) #if isinstance(x, complex) and isinstance(y, complex) else np.nan
+
+def Sqrt(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+     #   return np.sqrt(z)
+    return [np.sqrt(a) for a in z] if isinstance(z, list) else np.sqrt(z) #if isinstance(z, complex) else np.nan
+
+def Sqr(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    return z ** 2
+    return [a ** 2 for a in z] if isinstance(z, list) else z ** 2
+
+def Logn(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+#    if isinstance(z, complex):
+#        z=Real(z)
+    #if isinstance(z, complex):
+    try:
+        return [np.real(ctm.log(a)) if isinstance(a, complex) else np.nan for a in z] if isinstance(z, list) else Real(ctm.log(z)) 
+    except:
+        return nan
+
+def Exp(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.exp(z)
+    #    except:
+    #        return np.nan
+    
+    return [np.exp(a) for a in z] if isinstance(z, list) else np.exp(z) 
+
+def Sinh(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.sinh(z)
+    #    except:
+    #        return np.nan
+    return [np.sinh(a) for a in z] if isinstance(z, list) else np.sinh(z) 
+
+def Cosh(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.cosh(z)
+    #    except:
+    #        return np.nan
+    return [np.cosh(a) for a in z] if isinstance(z, list) else np.cosh(z) 
+
+def Tanh(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.tanh(z)
+    #    except:
+    #        return np.nan
+    return [np.tanh(a) for a in z] if isinstance(z, list) else np.tanh(z) 
+
+def Csch(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return 1 / np.sinh(z)
+    #    except:
+    #        return np.nan
+    return [1 / np.sinh(a) for a in z] if isinstance(z, list) else 1 / np.sinh(z) 
+
+def Sech(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return 1 / np.cosh(z)
+    #    except:
+    #        return np.nan
+    return [1 / np.cosh(a) for a in z] if isinstance(z, list) else 1 / np.cosh(z)
+
+def Coth(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.cosh(z) / np.sinh(z)
+    #    except:
+    #        return np.nan
+    return [np.cosh(a) / np.sinh(a) for a in z] if isinstance(z, list) else np.cosh(z) / np.sinh(z) 
+
+def Sign(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return float(np.sign(z))
+    #    except:
+    #        return np.nan
+    return [float(np.sign(a)) for a in z] if isinstance(z, list) else float(np.sign(z))
+
+def Cos(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    #if isinstance(z, complex):
+    #    z=Real(z)
+    #if isinstance(z, (float,int)):
+    #    try:
+    #        return np.cos(z)
+    #    except:
+    #        return np.nan
+    return [np.cos(a) for a in z] if isinstance(z, list) else np.cos(z) 
+
+def Sin(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.sin(z)
+    #    except:
+    #        return np.nan
+    #else:
+    return [np.sin(a) for a in z] if isinstance(z, list) else np.sin(z) #if isinstance(z, complex) else np.nan 
+
+    #return [np.sin(a) for a in z] #if isinstance(z, list) else np.sin(z) if isinstance(z, complex) else np.nan
+
+def Tan(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.tan(z)
+    #    except:
+    #        return np.nan
+    return [np.tan(a) for a in z] if isinstance(z, list) else np.tan(z)
+
+def Csc(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return 1 / np.sin(z)
+    #    except:
+    #        return np.nan
+    return [1 / np.sin(a) for a in z] if isinstance(z, list) else 1 / np.sin(z)
+
+def Sec(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return 1 / np.cos(z)
+    #    except:
+    #        return np.nan
+    return [1 / np.cos(a) for a in z] if isinstance(z, list) else 1 / np.cos(z) 
+
+def Cot(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.cos(z) / np.sin(z)
+    #    except:
+    #        return np.nan
+    return [np.cos(a) / np.sin(a) for a in z] if isinstance(z, list) else np.cos(z) / np.sin(z)
+
+def Asin(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.real(ctm.asin(z))
+    #    except:
+    #        return np.nan
+    return [np.real(ctm.asin(a)) if isinstance(a, complex) else np.nan for a in z] if isinstance(z, list) else np.real(ctm.asin(z))
+
+def Acos(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.real(ctm.acos(z))
+    #    except:
+    #        return np.nan
+    return [np.real(ctm.acos(a)) if isinstance(a, complex) else np.nan for a in z] if isinstance(z, list) else np.real(ctm.acos(z))
+
+def Abs(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    #if isinstance(z, complex):
+    #    z=Real(z)
+    #if isinstance(z, complex):
+    #    return abs(z)
+    return [abs(a) for a in z] if isinstance(z, list) else abs(z)
+
+def Real(z):
+    try:
+        c=[np.real(a) for a in z]
+    except:
+        c=np.real(z)
+    return c
+    #try:
+    #    z=all(z)
+    #except:
+    #    z=z
+    #if isinstance(z, str):
+    #    z = Transltr(z)
+    #return [np.real(a) for a in z] if isinstance(z, list) else np.real(z)
+
+def Atanr(z):
+    if isinstance(z, str):
+        z = Transltr(z)
+    if isinstance(z, complex):
+        z=Real(z)
+    #if isinstance(z, complex):
+    #    try:
+    #        return np.real(ctm.atan(z))
+    #    except:
+    #        return np.nan
+    return [np.real(ctm.atan(a)) if isinstance(a, complex) else np.nan for a in z] if isinstance(z, list) else np.real(ctm.atan(z))
+
+def Norm(z):
+    if isinstance(z, str):
+        a = Transltr(z)
+    else:
+        a = np.linalg.norm(z)
+    #if isinstance(z, complex):
+    #    return np.real(np.linalg.norm(z))
+    #return [float(np.linalg.norm(z)) for a in z] if isinstance(z, list) else np.linalg.norm(z)
+    return a
---
+++ NoteBooks/Model.py
@@ -0,0 +1,95 @@
+import sys
+sys.path.insert(0, '~/libs')
+sys.path.insert(1, '~/Simulation')
+from Math import Plus, Minus, Divide, Times,Power, Maximum, Minimum, Atan2r
+from Math import Sqrt, Sqr, Logn, Exp, Sinh, Cosh, Tanh, Csch, Sech, Coth, Cos, Sin, Tan, Csc, Sec, Cot, Asin, Acos, Norm, Abs, Real, Atanr
+import numpy as np
+from math import nan as NaN
+from math import inf as Inf
+import cmath
+
+def IndividualTest(u,indR1,r,u0):
+    r1=r[0]#Radio del robot 1
+    r2=r[1]#Radio del robot 2
+    rRb1=r1+r2#Consideración de ambos radios
+    q1=[u[0],u[1]]#Posicion del robo 1
+    q2=[u[2],u[3]]#Posicion del robot x
+    qx2=q2[0]#Posicion en X del robot 2 
+    qy2=q2[1]#Posicion en y del robot 2
+    qx1=q1[0]#Posicion en x del robot 1
+    qy1=q1[1]#Posicion en y del robot 1
+    xd1=u[4][0]#Posicion deseada del robot 1 en x
+    xdp1=u[10]#Componente de Velocidad deseada del robot 1 en x
+    yd1=u[4][1]#Posicion deseada del robot 1 en y
+    ydp1=u[11]#Componente de Velocidad deseada del robot 1 en y
+    xd2=u[5][0]
+    xdp2=u[12] 
+    yd2=u[5][1] 
+    ydp2=u[13]
+    Vx1=u[6]
+    Vy1=u[7]
+    Vx2=u[8]
+    Vy2=u[9]
+    qd_p1=[xdp1,ydp1]
+    qd_p2=[xdp2,ydp2]
+    qd1=[xd1,yd1]
+    qd2=[xd2,yd2]
+    V1=[Vx1,Vy1]
+    V2=[Vx2,Vy2]
+    t1=u[14]
+    t2=u[15]
+    tR=[t1,t2]
+    td=u[16]
+    tdR=[td,td]
+        
+    OrR2R1=Plus(q1,Divide(Times(rRb1,Minus(q2,q1)),Norm(Minus(q2,q1))))
+    OrR1R1=Plus(q2,Divide(Times(rRb1,Minus(q1,q2)),Norm(Minus(q1,q2))))
+
+    K_1=[1,1]
+    K_2=[1,1]
+    #K_1=[1,1,1,1,1,1,1,1]
+    #K_2=[1,1,1,1,1,1,1,1]
+    individual=str(indR1)
+    try:
+        vel_u=Real(eval(individual.replace('"', '').replace("'", '')))
+    except ValueError:#Excepcion para controlar una mala traduccion del individo, solo por precaucion(En teoria no deberia ocurrir nunca)
+        sys.exit()
+    ##########################################################################
+    vR1b=Real((cmath.log(np.abs(cmath.log(Cos(cmath.log(np.linalg.norm(Minus(q1,OrR1R1)))))))*cmath.log(np.linalg.norm(Minus(q1,OrR1R1)))))+Real((cmath.log(np.abs(cmath.log(Cos(cmath.log(np.linalg.norm(Minus(q2,OrR2R1)))))))*cmath.log(np.linalg.norm(Minus(q2,OrR2R1)))))
+    
+    #vR1b=(np.real(cmath.log(np.abs(cmath.log(cmath.cos(cmath.log(np.linalg.norm(Minus(q1,OrR1R1)))))))*cmath.log(np.linalg.norm(Minus(q1,OrR1R1)))))+(np.real(cmath.log(np.abs(cmath.log(cmath.cos(cmath.log(np.linalg.norm(Minus(q2,OrR2R1)))))))*cmath.log(np.linalg.norm(Minus(q2,OrR2R1)))))
+    #############################################################################
+
+    vR2=Plus(vel_u,vR1b)
+    vR1=Plus(vel_u,vR1b)
+    vel_1=[ModelOfRobot(vR1,K_1,qd_p1,qd1,q1)]
+    vel_2=[ModelOfRobot(vR2,K_2,qd_p2,qd2,q2)]
+    #vel_1=[OmniModel(vR1,K_1,qd_p1,qd1,q1)]
+    #vel_2=[OmniModel(vR2,K_2,qd_p2,qd2,q2)]
+    return vel_1,vel_2
+
+def ModelOfRobot(Vel_u,K,vel_des,pos_des,pos_q):
+    xd_p=vel_des[0]
+    yd_p=vel_des[1]
+    x_d=pos_des[0]
+    y_d=pos_des[1]
+    q_x=pos_q[0]
+    q_y=pos_q[1]
+    k_1=K[0]
+    k_2=K[1]
+    VxR_a=xd_p+k_1*(x_d-q_x)
+    VyR_b=yd_p+k_2*(y_d-q_y)
+    #aux=isinstance(Vel_u,(float,np.float64))
+    if (isinstance(Vel_u,(float,np.float64))==False):
+        Vx_u=Vel_u[0]
+        Vy_u=Vel_u[1]
+        VxR_r=Times(Vx_u,(y_d-q_y))
+        VyR_r=Times(Times(-1,Vy_u),(x_d-q_x))
+    else:
+        VxR_r=Times(Vel_u,(y_d-q_y))
+        VyR_r=Times(Times(-1,Vel_u),(x_d-q_x))
+
+    vxR1=Plus(VxR_a,VxR_r)
+    vyR1=Plus(VyR_b,VyR_r)
+    Vel_R=list([vxR1,vyR1])
+    return Vel_R
